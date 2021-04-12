8a0600297df5a02922470acb7d2f36ef496372b5
Jonathon Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2021-04-12 19:43:43 -0400

    Added a proc to read in the int in any radix
    only needs to convert the string of chars into a single usable int

---
+++ Project2.asm
@@ -0,0 +1,125 @@
+; Project2.asm - This program inputs two values, and allows the user to perform arithmetic operations between them.
+;The input and output radices can be set to be different.
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+Include irvine32.inc
+
+.data
+valueA DB ?
+valueB DB ?
+input1 DB "Please enter an input radix: (Allowed Inputs: (0-9)(a-z)(A-Z) for 0d-62d)",0ah,0dh,0
+input2 DB "Please the number A:",0ah,0dh,0
+input3 DB "Please the number B:",0ah,0dh,0
+inputRadix DB ?
+outputRadix DB ?
+
+.code
+	main proc
+
+	call readnum
+	
+	
+
+	endOfProgram:
+
+	
+	INVOKE ExitProcess, 0
+
+main endp
+
+;----------------------------------------------------------------------------------------------------------------------------------------------
+;ReadNum
+;Receives: 
+;Reads a number in any radix and converts the number into an int and returns it to EAX
+;----------------------------------------------------------------------------------------------------------------------------------------------
+
+ReadNum PROC
+
+.data
+
+	number db 33 dup(0);
+
+	askRadix db 'Input Radix: ', 0
+	newLine db 13,10,0
+	askNum db 'Input number, press enter once done', 13,10,0
+	oopsie db ' is larger than radix so that number doesnt make sense its like saying g11 hex, try again but be better', 13,10,0
+
+.code
+
+	pushad
+
+	;Get input for radix and number
+	mov edx, offset askRadix
+	call writestring
+	call readchar
+	call writechar
+	mov bl, al
+	mov edx, offset newLine
+	call writestring
+	mov edx, offset askNum
+	call writestring
+	mov ecx, 32
+	mov esi, offset number
+
+Read_Loop:
+	
+	call readchar
+	call writechar
+	jmp Fix_Num_Val
+Turned_To_Num:
+	cmp al, bl
+	jge Error
+	mov [esi], al
+	inc esi
+	jcxz Turn_To_Int
+	dec cx
+	jmp Read_Loop
+
+Fix_Num_Val:
+
+		cmp al, 57
+		jg Letter
+		cmp al, 48
+		jl Error
+		sub al, '0'
+		jmp Turned_To_Num
+
+	Letter:
+
+		cmp al, 90
+		jg Lowercase
+		cmp al, 65
+		jl Error
+		sub al, 29
+		jmp Turned_To_Num
+
+	Lowercase:
+
+		cmp al, 122
+		jg Error
+		cmp al, 97
+		jl Error
+		sub al, 87
+		jmp Turned_To_Num
+
+Error:
+
+	mov edx, offset newLine
+	call writestring
+	call writechar
+	mov edx, offset oopsie
+	call writestring
+	mov esi, offset number
+	jmp Read_Loop
+
+Turn_To_Int:
+
+	popad
+
+ReadNum ENDP
+
+end main
